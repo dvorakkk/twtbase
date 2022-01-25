b18967d7cb3849430a3d940c79f56d8a244dcb27
ammm <90204443+dvorakkk@users.noreply.github.com>
2022-01-25 14:45:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-consumer_key = "3bH6kWin1zcAAZlZiwAWI6oGE"
-consumer_secret = "MAn1ljydiZFjTh8HDLUPs7o7KnVs3d2S0Is6QxYoXjnx56uxIw"
-access_token = "1427148449519046659-YIKSXz9aU3uJTnYcnBgTIq32xAIDsH"
-access_token_secret = "FNB7dEtsWvrG2YCftRhk0tdbiJsHdczHJ0R30Vf8zl7TW"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 trigger = "-lurr"
 timezone = 7 #change this into your timezone. for example if your timezone is GMT+7 you put 7
